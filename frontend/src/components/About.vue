87f5dd200d091b63b96c015eaf7457259ea0bbf1
Muhammad Arslan <m.arslan.0011333@gmail.com>
2023-07-23 21:09:05 +0500

    improvements: responsiveness handled and finalised in complete app

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="main-bg">
     <Navbar />
-    <div class="custom-spacing-container">
+
+    <!-- For Desktop Web Screen -->
+    <div v-if="$vuetify.display.lgAndUp" class="custom-spacing-container">
       <div class="text-white text-center font-main-title txt100">
         Password <span class="text-deep-orange font-main-title txt100">Manager</span>
       </div>
@@ -21,6 +23,31 @@
         <img :src="youtube" alt="img" class="custom-image-size ml-10"/>
       </div>
     </div>
+
+    <!-- For Tablet and Mobile Screen -->
+    <div
+      v-else
+      class="d-flex flex-column align-center"
+      :class="$vuetify.display.smAndUp ? 'custom-home-tab' : 'custom-home-mob'">
+      <div>
+        <img :src="fingerprint" alt="img" class="custom-logo-size"/>
+      </div>
+      <br>
+      <div class="text-white text-center font-sans-serif txt16 mb-0">
+        It is designed to alleviate the burden of managing and
+      </div>
+      <span
+        class="text-deep-orange font-sans-serif mt-0"
+        :class="$vuetify.display.smAndUp ? 'txt24' : 'txt20'"
+      >
+        securing multiple passwords
+      </span>
+      <div class="d-flex flex-column">
+        <img :src="fb" alt="img" class="custom-tab-mob-image-size mt-16 mb-10"/>
+        <img :src="amazon" alt="img" class="custom-tab-mob-image-size mb-10"/>
+        <img v-if="$vuetify.display.smAndUp" :src="youtube" alt="img" class="custom-tab-mob-image-size mb-10"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,6 +56,7 @@ import Navbar from "../layouts/Navbar.vue"
 import fb from "../assets/images/fb.png"
 import amazon from "../assets/images/amazon.png"
 import youtube from "../assets/images/youtube.png"
+import fingerprint from "../assets/images/print.png"
 
 export default {
   components: {
@@ -38,7 +66,8 @@ export default {
     return {
       fb,
       amazon,
-      youtube
+      youtube,
+      fingerprint
     };
   },
 };
@@ -74,10 +103,26 @@ export default {
 .v-responsive {
   max-width: 230px
 }
+.custom-home-tab {
+  padding: 150px 50px;
+}
+.custom-home-mob {
+  padding: 70px 50px;
+}
 .custom-image-size {
   height: 100px;
   width: 340px;
   border-radius: 20px;
+}
+.custom-tab-mob-image-size {
+  height: 90px;
+  width: 270px;
+  border-radius: 20px;
+}
+.custom-logo-size {
+  height: 100px;
+  width: 100px;
+  border-radius: 6px;
 }
 .custom-mt {
   margin-top: 220px;
--- frontend/src/layouts/Navbar.vue
+++ frontend/src/layouts/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="d-flex justify-space-between">
     <h1 v-if="$vuetify.display.sm || $vuetify.display.md || $vuetify.display.lg || $vuetify.display.xl" class="text-white mt-3 ml-7 custom-text-size font-symbol">
-      Password <span class="text-deep-orange font-symbol"> Manager </span>
+      Password <span class="text-deep-orange font-avenir"> Manager </span>
     </h1>
-    <h1 v-else-if="$vuetify.display.xs" class="text-white mt-3 ml-7 custom-text-size font-avenir">
+    <h1 v-else-if="$vuetify.display.xs" class="text-white mt-3 ml-7 custom-text-size font-symbol">
         PS<span class="text-deep-orange font-avenir">Manager</span>
     </h1>
-    <div v-if="!token" class="text-white mt-6 mr-7 d-flex">
+    <div v-if="token" class="text-white mt-6 mr-7 d-flex">
       <span
         class="mr-6 cursor-pointer custom-primary-hover"
         @click="$router.push({ path: '/login' })"
@@ -20,7 +20,7 @@
         Signup
       </span>
     </div>
-    <div v-else class="text-white mt-6 mr-4 d-flex">
+    <div v-else class="text-white mr-4 d-flex" :class="$vuetify.display.smAndUp ? 'mt-8' : 'mt-7'">
       <span
         class="mr-6 cursor-pointer custom-primary-hover"
         @click="$router.push({ path: '/dashboard' })"
